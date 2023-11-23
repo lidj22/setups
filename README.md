50c17804fe3ab547312db87691e9b62703c79520
lidj22 <36579697+lidj22@users.noreply.github.com>
2023-11-23 00:08:26 -0800

    virtualization

--- README.md
+++ README.md
@@ -3,9 +3,20 @@
 Setup scripts.
 
 ## Ubuntu
+
+Development script
 ```sh
 sudo su
-wget https://raw.githubusercontent.com/lidj22/setups/main/ubuntu.sh \
+wget -O ubuntu.sh https://raw.githubusercontent.com/lidj22/setups/main/ubuntu-22/develop.sh \
+    && chmod +x ubuntu.sh \
+    && ./ubuntu.sh \
+    && rm ubuntu.sh
+```
+
+Virtualization script
+```sh
+sudo su
+wget -O ubuntu.sh https://raw.githubusercontent.com/lidj22/setups/main/ubuntu-22/virtualization.sh \
     && chmod +x ubuntu.sh \
     && ./ubuntu.sh \
     && rm ubuntu.sh
---
+++ ubuntu-22/virtualization.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+# virtualbox
+wget -O virtualbox.deb https://download.virtualbox.org/virtualbox/7.0.12/virtualbox-7.0_7.0.12-159484~Ubuntu~jammy_amd64.deb \
+    && apt-get --fix-broken install virtualbox.deb \
+    && rm virtualbox.deb
+
+# vagrant
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+apt update && apt install vagrant
