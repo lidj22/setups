0d463f8f46829ce891b4baaf11eca3f6bf9b45d4
lidj22 <36579697+lidj22@users.noreply.github.com>
2023-11-23 00:51:57 -0800

    test

--- ubuntu-22/virtualization.sh
+++ ubuntu-22/virtualization.sh
@@ -3,11 +3,11 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # virtualbox
-wget -O virtualbox.deb https://download.virtualbox.org/virtualbox/7.0.12/virtualbox-7.0_7.0.12-159484~Ubuntu~jammy_amd64.deb \
-    && dpkg -i virtualbox.deb \
-    && apt-get --fix-broken install -y \
-    && dpkg -i virtualbox.deb \
-    && rm virtualbox.deb
+wget -O virtualbox.deb https://download.virtualbox.org/virtualbox/7.0.12/virtualbox-7.0_7.0.12-159484~Ubuntu~jammy_amd64.deb
+dpkg -i virtualbox.deb
+apt-get --fix-broken install -y
+dpkg -i virtualbox.deb
+rm virtualbox.deb
 
 # vagrant
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
