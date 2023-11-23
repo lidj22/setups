37eaded26769ee8c6bcbdd55331436ff55e8478a
lidj22 <36579697+lidj22@users.noreply.github.com>
2023-11-23 00:35:47 -0800

    fix

--- ubuntu-22/virtualization.sh
+++ ubuntu-22/virtualization.sh
@@ -5,7 +5,7 @@ export DEBIAN_FRONTEND=noninteractive
 # virtualbox
 wget -O virtualbox.deb https://download.virtualbox.org/virtualbox/7.0.12/virtualbox-7.0_7.0.12-159484~Ubuntu~jammy_amd64.deb \
     && dpkg -i virtualbox.deb \
-    && apt-get --fix-broken \
+    && apt-get --fix-broken install \
     && dpkg -i virtualbox.deb \
     && rm virtualbox.deb
 
